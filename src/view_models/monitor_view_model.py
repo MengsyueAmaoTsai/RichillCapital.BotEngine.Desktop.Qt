48f1636b22f05ebf54ab25058fb15ea0a92d8453
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-10-20 15:29:31 +0800

    emit signal

--- src/view_models/monitor_view_model.py
+++ src/view_models/monitor_view_model.py
@@ -2,11 +2,12 @@
 
 import asyncio
 from pathlib import Path
-
+from datetime import datetime as DateTime
 from aiofile import AIOFile, LineReader
 from PySide6.QtCore import Property, QFileSystemWatcher, QObject, Signal, Slot
+from aiohttp import ClientSession
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler, FileModifiedEvent, DirModifiedEvent
 
 class MonitorViewModel(QObject, FileSystemEventHandler):
     new_signal = Signal()
@@ -20,17 +21,53 @@ class MonitorViewModel(QObject, FileSystemEventHandler):
         _ = self.observer.schedule(self, "C://BotEngineDesktop//Data", recursive=False)
         self.observer.start()
     
-    def on_modified(self, event):
-        path = Path(event.src_path)
-
-        asyncio.run(self.read_file_async(path))
-
-    async def read_file_async(self, path: Path) -> None:
-        
-        if path.suffix not in [".sig"]:
+    def on_modified(self, event: FileModifiedEvent | DirModifiedEvent):
+        if isinstance(event, DirModifiedEvent): 
             return
+        
+        if event.event_type != "modified":
+            return 
+        
+        path = Path(event.src_path)
+        raw_data = asyncio.run(self.read_file_async(path))
+        
+        if (len(raw_data) == 0):
+            return
+        
+        data = raw_data[-1]
+        parts = data.split(",")
+
+        time = parts[0]
+        trade_type = parts[1] 
+        symbol = parts[2] 
+        quantity = float(parts[3]) 
+        price = float(parts[3]) 
+
+        signal = {
+            "time": time,
+            "tradeType": trade_type,
+            "symbol": symbol,
+            "quantity": quantity,
+            "price": price
+        }
+        asyncio.run(self.emit_signal_async(path.stem, signal))
+
+    async def emit_signal_async(self, bot_id: str, signal):
+        async with ClientSession() as session:
+            async with session.post(f"http://localhost:10005/api/bots/{bot_id}/signals", data=signal) as response:
+                content = await response.text()
+                print(content)
+
+    async def read_file_async(self, path: Path) -> list[str]:
+        lines: list[str] = []
+
+        if path.suffix not in [".sig"]:
+            return lines
         
         async with AIOFile(path, "r", encoding="utf-8") as file:
             async for line in LineReader(file):
-                print(line)        
+                if (line.strip().isspace() or line.strip() == ""):
+                    continue
+                lines.append(str(line))
+        return lines        
     
